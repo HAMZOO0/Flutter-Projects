ed790017b522d3940a5339757b3ec21d4c8361d3
Hamza <hamzakhan174722@gmail.com>
2025-04-26 20:42:02 +0500

    feat(ui): implement app bar with refresh button in WeatherScreen

--- Weather App/lib/weather_screen.dart
+++ Weather App/lib/weather_screen.dart
@@ -4,6 +4,30 @@ class WeatherScreen extends StatelessWidget {
   const WeatherScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Weather App",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            // IconButton more good click effect
+            onPressed: () {
+              print('refresh btn clicked');
+            },
+            icon: const Icon(Icons.refresh),
+          ),
+          // InkWell(
+          //   // we use guesture also on the place of inkwell it just give us the click effect color
+          //   onTap: () {
+          //     print("refresh clicked");
+          //   },
+          //   child: const Icon(Icons.refresh),
+          // ),
+        ],
+      ),
+    );
   }
 }
